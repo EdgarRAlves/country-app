79d8a648a4e44d51f10d011ba94c4d3168060ca4
Edgar <8x.edgar@gmail.com>
2022-08-11 15:14:59 +0300

    pagination added

--- app/src/Controller/CountryController.php
+++ app/src/Controller/CountryController.php
@@ -2,8 +2,9 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
@@ -11,46 +12,62 @@ use Symfony\Contracts\HttpClient\HttpClientInterface;
 class CountryController extends AbstractController
 {
     private $client;
+    private $paginator;
 
-    public function __construct(HttpClientInterface $client)
+    public function __construct(HttpClientInterface $client, PaginatorInterface $paginator)
     {
         $this->client = $client;
+        $this->paginator = $paginator;
     }
 
     #[Route('/countries', name: 'countries')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
         $response = $this->client->request(
             'GET',
             'https://restcountries.com/v2/all?fields=name,population,region'
         );
+        $content = $response->toArray();
+
+        $pagination = $this->paginator->paginate(
+            $content,
+            $request->query->getInt('page', 1),
+            $request->query->getInt('limit', 12)
+        );
 
         return $this->render(
             'country/index.html.twig',
             [
-                'countries' => $response->toArray()
+                'countries' => $pagination
             ]
         );
     }
 
     #[Route('/countries/Europe', name: 'europe')]
-    public function countriesFromEurope(): Response
+    public function countriesFromEurope(Request $request): Response
     {
         $response = $this->client->request(
             'GET',
             'https://restcountries.com/v2/region/Europe?fields=name,population,region'
         );
+        $content = $response->toArray();
+
+        $pagination = $this->paginator->paginate(
+            $content,
+            $request->query->getInt('page', 1),
+            $request->query->getInt('limit', 12)
+        );
 
         return $this->render(
             'country/europe.html.twig',
             [
-                'countries' => $response->toArray()
+                'countries' => $pagination
             ]
         );
     }
 
     #[Route('/countries/smallerThanLithuania', name: 'smaller_lithuania')]
-    public function countriesSmallerThanLithuania(): Response
+    public function countriesSmallerThanLithuania(Request $request): Response
     {
         $response = $this->client->request(
             'GET',
@@ -70,10 +87,16 @@ class CountryController extends AbstractController
             }
         }
 
+        $pagination = $this->paginator->paginate(
+            $result,
+            $request->query->getInt('page', 1),
+            $request->query->getInt('limit', 12)
+        );
+
         return $this->render(
             'country/smaller_lithuania.html.twig',
             [
-                'countries' => $result
+                'countries' => $pagination
             ]
         );
     }
