3c4edf3ab4c3af2e76b232ea6a1515dee48895ce
Mindaugas Kaseta <wwwemka@gmail.com>
2022-09-14 08:04:51 +0000

    Some tips

--- app/src/Controller/CountryController.php
+++ app/src/Controller/CountryController.php
@@ -12,15 +12,15 @@ class CountryController extends AbstractController
 {
     private $countryModel;
 
-    public function __construct(CountryModel $countryModel)
+    public function __construct(CountryModel $countryModel) // What if there would be more than one Data provider , for ex. - now you think about database/cache - Just think about this Controller, how to change so that this code would not care who is providing data :) Think about decorator pattern
     {
-        $this->countryModel = $countryModel;
+        $this->countryModel = $countryModel; // Also, there is another way to get rid of constructor - think about autowiring ;)
     }
 
     #[Route('/countries', name: 'countries')]
     public function index(Request $request): Response
     {
-        $sort = $request->query->getAlpha('sort');
+        $sort = $request->query->getAlpha('sort'); //well it's up to you bet there is other ways to get request :) Just think - if you need to test this code - how you test would look
         $direction = $request->query->getAlpha('direction');
         $filter = $request->query->getAlpha('filter');
         $page = $request->query->getInt('page', 1);
@@ -28,12 +28,12 @@ class CountryController extends AbstractController
 
         $content = $this->countryModel->getCountries($sort, $direction, $filter);
 
-        $pagination = $this->countryModel->paginate($content, $page, $limit);
+        $pagination = $this->countryModel->paginate($content, $page, $limit); // Ok let's think what would happen if you want to create one more function which returns cities ? You would create one more model which contains another paginate function ? DRY, KISS
 
         return $this->render(
             'country/index.html.twig',
             [
-                'countries' => $pagination,
+                'countries' => $pagination, // Bad naming sense :D
             ]
         );
     }
